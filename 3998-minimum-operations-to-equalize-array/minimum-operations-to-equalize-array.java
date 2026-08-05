c65c33820849e6448c9fefd8e551e78dcd33356d
kabilan <kabilroyitofficial@gmail.com>
2026-08-05 16:05:11 +0530

    Time: 1 ms (68.39%) | Memory: 45.3 MB (20.36%) - LeetSync

---
+++ 3998-minimum-operations-to-equalize-array/minimum-operations-to-equalize-array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minOperations(int[] nums) {
+        int count = 0;
+        for(int i = 0;i<nums.length-1;i++)
+        {
+            if(nums[i]!=nums[i+1]){
+                count++;
+                break;
+            }
+        }
+        return count;
+    }
+}
