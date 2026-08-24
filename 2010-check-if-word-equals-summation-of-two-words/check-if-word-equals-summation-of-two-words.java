e36bcff0bca52a27ad93c896567015e448b1559e
kabilan <kabilroyitofficial@gmail.com>
2026-08-24 15:58:46 +0530

    Time: 6 ms (2.36%) | Memory: 43.3 MB (11.81%) - LeetSync

---
+++ 2010-check-if-word-equals-summation-of-two-words/check-if-word-equals-summation-of-two-words.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isSumEqual(String firstWord, String secondWord, String targetWord) {
+        
+        String a = "";
+        String b = "";
+        String c = "";
+        for(int i = 0;i<firstWord.length();i++)
+        {
+            a+=firstWord.charAt(i)-'a';
+        }
+
+        for(int j = 0;j<secondWord.length();j++)
+        {
+            b+=secondWord.charAt(j)-'a';
+        }
+        
+        for(int k = 0;k<targetWord.length();k++)
+        {
+            c+=targetWord.charAt(k)-'a';
+        }
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        long a1 = Long.parseLong(a);
+        long b1 = Long.parseLong(b);
+        long c1 = Long.parseLong(c);
+        long sum = a1+b1;
+        return sum == c1;
+    }
+}
